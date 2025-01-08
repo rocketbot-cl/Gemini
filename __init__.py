a5a5bb93114657611f2d0fd1e1b6c1dd945f31f1
nico-garcia-r <nicolas.garcia@rocketbot.com>
2025-01-08 13:00:29 -0300

    [new] first commit. Add commands to connect and generate content.

---
+++ __init__.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+"""
+Base para desarrollo de modulos externos.
+Para obtener el modulo/Funcion que se esta llamando:
+     GetParams("module")
+
+Para obtener las variables enviadas desde formulario/comando Rocketbot:
+    var = GetParams(variable)
+    Las "variable" se define en forms del archivo package.json
+
+Para modificar la variable de Rocketbot:
+    SetVar(Variable_Rocketbot, "dato")
+
+Para obtener una variable de Rocketbot:
+    var = GetVar(Variable_Rocketbot)
+
+Para obtener la Opcion seleccionada:
+    opcion = GetParams("option")
+
+
+Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
+
+    pip install <package> -t .
+
+"""
+import os, sys
+
+GetParams = GetParams # type: ignore
+SetVar = SetVar # type: ignore
+PrintException = PrintException # type: ignore
+
+base_path = tmp_global_obj["basepath"] # type: ignore
+cur_path = base_path + 'modules' + os.sep + 'Gemini' + os.sep + 'libs' + os.sep
+
+
+if cur_path not in sys.path:
+    sys.path.append(cur_path)
+
+import google.generativeai as genai # type: ignore
+
+global mod_model_Gemini
+module = GetParams("module")
+
+try:
+    if module == "connect":
+        try:
+            api_key = GetParams("api_key")
+            result = GetParams("result")
+
+            genai.configure(api_key=api_key)
+            get_model = genai.get_model("models/gemini-1.5-flash-latest")
+            mod_model_Gemini = genai.GenerativeModel("gemini-1.5-flash")
+            SetVar(result, True)
+
+        except Exception as e:
+            SetVar(result, False)
+            raise e
+        
+    if module != "connect" and not mod_model_Gemini:
+        raise Exception("Please connect to Gemini AI before using this module")
+
+    if module == "generate_content":
+        try:
+            prompt = GetParams("prompt")
+            result = GetParams("result")
+
+            response = mod_model_Gemini.generate_content(prompt)
+            SetVar(result, response.text)
+
+        except Exception as e:
+            SetVar(result, False)
+            raise e
+        
+    
+        
+except Exception as e:
+    PrintException()
+    raise e
---
+++ docs/Manual_Gemini.pr.md
@@ -0,0 +1,31 @@
+# Gemini
+  
+Este módulo permite trabalhar com a API de IA do Google Gemini  
+
+*Read this in other languages: [English](Manual_Gemini.md), [Português](Manual_Gemini.pr.md), [Español](Manual_Gemini.es.md)*
+  
+![banner](imgs/Banner_Gemini.png o jpg)
+## Como instalar este módulo
+  
+Para instalar o módulo no Rocketbot Studio, pode ser feito de duas formas:
+1. Manual: __Baixe__ o arquivo .zip e descompacte-o na pasta módulos. O nome da pasta deve ser o mesmo do módulo e dentro dela devem ter os seguintes arquivos e pastas: \__init__.py, package.json, docs, example e libs. Se você tiver o aplicativo aberto, atualize seu navegador para poder usar o novo módulo.
+2. Automático: Ao entrar no Rocketbot Studio na margem direita você encontrará a seção **Addons**, selecione **Install Mods**, procure o módulo desejado e aperte instalar.  
+
+
+## Descrição do comando
+
+### Conectar ao Gemini
+  
+Conecte-se à API do Google Gemini
+|Parâmetros|Descrição|exemplo|
+| --- | --- | --- |
+|Chave da API|Chave da API|AIza....|
+|Atribuir resultado à variável|Variável onde o resultado da conexão será armazenado|result|
+
+### Gerar Conteúdo
+  
+Gere conteúdo fornecendo um prompt das informações que deseja
+|Parâmetros|Descrição|exemplo|
+| --- | --- | --- |
+|Prompt|Texto que será usado como prompt para gerar o conteúdo|O que é Rocketbot?|
+|Atribuir resultado à variável|Variável onde o resultado da execução será armazenado|result|
